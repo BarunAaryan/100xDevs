85fa95089dfc4183cccc8a4589e6c180b3815e52
Barun Aaryan <barunaryan1@gmail.com>
2024-06-12 00:17:03 +0530

    learnt about interfaces

---
+++ Week_9.2/node-app/a.js
@@ -0,0 +1,9 @@
+"use strict";
+// function greet(firstName: string){
+// console.log("Hello", firstName)
+// }
+// greet("Barun")
+function sum(a, b) {
+    return a + b;
+}
+console.log(sum(4, 5));
---
+++ Week_9.2/node-app/a.ts
@@ -0,0 +1,37 @@
+// function greet(firstName: string){
+// console.log("Hello", firstName)
+// }
+// greet("Barun")
+
+// function sum(a: number, b: number): number {
+//     return a+b;
+// }
+
+// console.log(sum(4,5))
+
+
+// interface User{
+//     firstName: string;
+//     lastName: string;
+//     age: number
+//     email?: string;
+//     //email is optional user can pass or not it is his wish
+// };
+
+// function isLegal(user: User){
+//     if(user.age > 18){
+//         return true;
+//     }else{
+//         return false;
+//     }
+// }
+
+// function greet(user: User){
+//     console.log("Hi There" + user.firstName)
+// }
+
+// isLegal({
+//     firstName: "Barun",
+//     lastName: "Aaryan",
+//     age: 22
+// })
