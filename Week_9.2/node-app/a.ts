358401ab2dee05251dcb94ecfd23e4442af16f08
Barun Aaryan <barunaryan1@gmail.com>
2024-06-14 01:55:29 +0530

    TypeScript

--- Week_9.2/node-app/a.ts
+++ Week_9.2/node-app/a.ts
@@ -35,3 +35,39 @@
 //     lastName: "Aaryan",
 //     age: 22
 // })
+
+
+//enumerations
+// type KeyInput = "up" | "down" | "left" | "right";
+// enum Direction{
+//     Up= "up", //0 //defining of enums
+//     Down= "down" , //1
+//     Left="left", //2
+//     Right="right" //3 
+// }
+
+// function doSomething(keyPressed: Direction){
+//     //do something
+//     if(keyPressed == Direction.Up){
+
+//     }
+// }
+
+// doSomething(Direction.Right);
+
+//generics
+type Input = number | string;
+
+function firstEL(arr: Input[]){
+    return arr[0];
+}
+const value= firstEL(["Barun"]);
+
+//using generics
+function identity<T>(arr: T[]){
+    return arr[0];
+}
+let output1= identity<string>(["MyString"]);
+let output2 = identity<number>([100]);
+
+output1.toUpperCase();
