77bb756a223d048410491ee1771d310950feee87
Barun Aaryan <barunaryan1@gmail.com>
2024-06-16 16:53:22 +0530

    Intro to NextJS

---
+++ Week_14_Nextjs/next-app/app/(auth)/layout.tsx
@@ -0,0 +1,9 @@
+import React from "react"
+
+export default function({children} : {children : React.ReactNode}){
+    return <div className="border-b text-center">
+        20% off for the next 3 days
+        {children}
+    </div>
+ 
+} 
