e4f16a4f9d8fd07ce39967064f67cd17f71bc282
Barun Aaryan <barunaryan1@gmail.com>
2024-06-25 21:18:44 +0530

    NEXTJS backend till step 6

---
+++ week_14-2_nextjs_server/app/api/user/route.ts
@@ -0,0 +1,15 @@
+export async function GET(){
+    //database logic 
+    return Response.json({
+    email: "barunaryna1@gmail.com",
+    name: "Barun"
+    })
+}  
+
+// export function POST(){
+//     //database logic 
+//     return Response.json({
+//     email: "barunaryna1@gmail.com",
+//     name: "Barun"
+//     })
+// } 
