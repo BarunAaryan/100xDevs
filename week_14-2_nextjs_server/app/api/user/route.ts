72939b44c08e4426816c9a61a749d699d9c66627
Barun Aaryan <barunaryan1@gmail.com>
2024-06-29 11:36:31 +0530

    Backend Part1

--- week_14-2_nextjs_server/app/api/user/route.ts
+++ week_14-2_nextjs_server/app/api/user/route.ts
@@ -1,15 +1,30 @@
-export async function GET(){
-    //database logic 
-    return Response.json({
-    email: "barunaryna1@gmail.com",
-    name: "Barun"
-    })
-}  
+import { NextRequest } from "next/server"
+import {PrismaClient} from "@prisma/client"
+const client = new PrismaClient();
 
-// export function POST(){
+// export async function GET(){
 //     //database logic 
 //     return Response.json({
 //     email: "barunaryna1@gmail.com",
 //     name: "Barun"
 //     })
-// } 
+// }  
+
+export async function POST(req:NextRequest){
+    //extract the body
+const body= await req.json();
+await client.user.create({
+    data:{
+        username: body.username,
+        password: body.password
+    }
+})
+
+    //store the body in the database
+console.log(body);
+
+
+    return Response.json({
+    message: "You are logged in"
+    })
+} 
--- week_14-2_nextjs_server/app/page.tsx
+++ week_14-2_nextjs_server/app/page.tsx
@@ -1,25 +1,33 @@
-import axios from "axios";
-async function getUserDetails() {
-  //artificial waiting to load loader 
-  await new Promise((r)=> setTimeout(r, 5000))
-  const response = await axios.get("http://localhost:3000/api/user")
-  return response.data;
-}
+// import axios from "axios";
+// async function getUserDetails() {
+//   //artificial waiting to load loader 
+//   // await new Promise((r)=> setTimeout(r, 5000))
+//   const response = await axios.get("http://localhost:3000/api/user")
+//   return response.data;
+// }
 
 //async component -> only in server components
-export default async function Home() {
-  const userData = await getUserDetails();
-  return (
-    <div className="flex flex-col justify-center h-screen">
-    <div className="flex justify-center">
-        <div className="border p-8 rounded">
-            <div>
-                Name: {userData?.name}
-            </div>
+// export default async function Home() {
+//   const userData = await getUserDetails();
+//   return (
+//     <div className="flex flex-col justify-center h-screen">
+//     <div className="flex justify-center">
+//         <div className="border p-8 rounded">
+//             <div>
+//                 Name: {userData?.name}
+//             </div>
             
-            {userData?.email}
-        </div>
+//             {userData?.email}
+//         </div>
+//     </div>
+// </div>
+//   );
+// }
+
+export default function Home(){
+  return(
+    <div>
+      Hi There
     </div>
-</div>
-  );
+  )
 }
